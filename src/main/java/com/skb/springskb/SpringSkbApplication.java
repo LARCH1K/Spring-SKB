aa69c53b92649f427616cfab821f8e103c8c2277
LARCHIK <ovchinnikov20035696@gmail.com>
2024-02-28 10:52:46 +0500

    Init commit

---
+++ src/main/java/com/skb/springskb/SpringSkbApplication.java
@@ -0,0 +1,13 @@
+package com.skb.springskb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSkbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skb/springskb/SpringSkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skb.springskb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
